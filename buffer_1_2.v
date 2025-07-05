2073e814b74985d4ecf20661f393f00f7fa1e147
Senthilkumaran K <ksenthilkumaran30@gmail.com>
2025-07-05 12:24:10 +0530

    buffer_1_2.v

--- buffer_1_2.v
+++ buffer_1_2.v
@@ -1,13 +1,35 @@
 module buffer_1_2 (
     input wire clk,
     input wire rst,
-    input wire [7:0] data_in,
-    output reg [7:0] data_out
+    input wire [15:0] data_in,
+    input wire valid_in,
+    output reg ready_out,
+
+    output reg [15:0] data_out,
+    output reg valid_out,
+    input wire ready_in
 );
+
+    reg full;
+
     always @(posedge clk or posedge rst) begin
-        if (rst)
-            data_out <= 8'b0;
-        else
-            data_out <= data_in;
+        if (rst) begin
+            full <= 0;
+            data_out <= 0;
+            valid_out <= 0;
+            ready_out <= 1;
+        end else begin
+            if (valid_in && ready_out) begin
+                data_out <= data_in;
+                valid_out <= 1;
+                full <= 1;
+                ready_out <= 0;
+            end else if (ready_in && full) begin
+                valid_out <= 0;
+                full <= 0;
+                ready_out <= 1;
+            end
+        end
     end
+
 endmodule
